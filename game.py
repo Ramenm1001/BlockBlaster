75d2fc5410499faf26db5ac1e2814902711fa1a4
Роббо Мастер <Ramenm1001@gmail.com>
2025-04-12 16:04:43 +1000

    draw

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 import pygame
 from negr import Negr
 from palma import Palma
+from housefornigga import HouseNigga
 
 win = pygame.display.set_mode((500, 500))
 negr = Negr(win, 200, 400, 50)
 
 
-
+island = [negr, ]
 run = True
 while run:
     for eve in pygame.event.get():
@@ -14,6 +15,7 @@ while run:
             run = False
 
     win.fill((0, 0, 0))
-
+    for some in island:
+        some.draw()
     pygame.display.update()
 pygame.quit()
