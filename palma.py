52fb4d5b137b3689f6b7da66773c35b3b42d9202
Роббо <gonozovdenis2010@gmail.co>
2025-04-12 15:48:53 +1000

    я добавил пальму

--- palma.py
+++ palma.py
@@ -1,6 +1,13 @@
+import pygame.draw
 class Palma:
     def __init__(self, win, x, y, tall):
         self.win = win
         self.x = x
         self.y = y
         self.tall = tall
+
+    def draw(self):
+        pygame.draw.rect(self.win,
+                         (170, 100, 15),
+                         (self.x, self.y,
+                          20, self.tall))
