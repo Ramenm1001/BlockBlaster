cc0f97da241ac4860a3766854b6b05f68c1a28e9
bober <arty.yanyshev2013@gmail.com>
2025-04-19 16:02:34 +1000

    bombombini gusini

--- housefornigga.py
+++ housefornigga.py
@@ -10,6 +10,7 @@ class HouseNigga:
         pygame.draw.rect(self.win, (170, 100, 15), (self.x, self.y, 100, self.tall))
         pygame.draw.rect(self.win, (10, 100, 15), (self.x, self.y-10, 10, self.tall))
     def recolor(self):
+        pass
 
     def update(self):
         self.draw()
