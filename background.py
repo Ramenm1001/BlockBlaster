105b9ca4d0f98c5e0663d959ea97557a85da41a7
bober <arty.yanyshev2013@gmail.com>
2025-04-19 16:32:05 +1000

    bombombini gusini

--- background.py
+++ background.py
@@ -1,4 +1,4 @@
 import pygame
 def draw_background(win):
         pygame.draw.rect(win, (9, 145, 12), (0, 0, 1000, 1000))
-        pygame.draw.rect(win, (64, 204, 251), (0, -200, 1000, 1000))
+        pygame.draw.rect(win, (64, 204, 251), (0, -900, 1000, 1000))
