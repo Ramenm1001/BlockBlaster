bc5455bc138f08f88e78def2e9f206a998a08682
bober <arty.yanyshev2013@gmail.com>
2025-04-19 16:40:28 +1000

    bombombini gusini

--- housefornigga.py
+++ housefornigga.py
@@ -7,10 +7,10 @@ class HouseNigga:
         self.tall = tall
 
     def draw(self):
-        pygame.draw.rect(self.win, (170, 100, 15), (self.x, self.y, 100, self.tall))
-        pygame.draw.rect(self.win, (120, 89, 7), (self.x - 5, self.y - 10, 110, 10))
-        pygame.draw.rect(self.win, (24, 64, 252), (self.x + 9, self.y + 6, 20, 20))
-        pygame.draw.rect(self.win, (24, 64, 252), (self.x + 72, self.y + 6, 20, 20))
+        pygame.draw.rect(self.win, (170, 100, 15), (self.x + 50, self.y, 100, self.tall))
+        pygame.draw.rect(self.win, (120, 89, 7), (self.x + 45, self.y - 10, 110, 10))
+        pygame.draw.rect(self.win, (24, 64, 252), (self.x + 59, self.y + 6, 20, 20))
+        pygame.draw.rect(self.win, (24, 64, 252), (self.x + 122, self.y + 6, 20, 20))
     def recolor(self):
         pass
 
