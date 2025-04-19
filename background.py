11133f8ca55f1818e57c730b3ff27da39e6923ae
bober <arty.yanyshev2013@gmail.com>
2025-04-19 16:31:18 +1000

    bombombini gusini

--- background.py
+++ background.py
@@ -1,20 +1,4 @@
-import pygame.draw
-class Background:
-    def __init__(self, win, x, y, tall):
-        self.win = win
-        self.x = x
-        self.y = y
-        self.tall = tall
-
-    def draw(self):
-        pygame.draw.rect(self.win, (9, 145, 12), (self.x, self.y, 1000, 1000))
-        pygame.draw.rect(self.win, (9, 145, 12), (self.x, self.y-200, 1000, 1000))
-
-
-
-
-    def recolor(self):
-        pass
-
-    def update(self):
-        self.draw()
+import pygame
+def draw_background(win):
+        pygame.draw.rect(win, (9, 145, 12), (0, 0, 1000, 1000))
+        pygame.draw.rect(win, (64, 204, 251), (0, -200, 1000, 1000))
